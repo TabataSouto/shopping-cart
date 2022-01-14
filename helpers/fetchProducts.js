8af004dd36ebc751d80d7ba923e4f6750194ef9d
Tabata <tabatac.souto@gmail.com>
2022-01-14 16:31:47 -0300

    Requisito 01: crie uma listagem de produtos [resolvido]

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
+const fetchProducts = async () => {
   // seu código aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
