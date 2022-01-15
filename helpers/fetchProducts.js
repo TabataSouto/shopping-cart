a1624cd754a364a181d2f052daeeda1a4038fa4d
Tabata <tabatac.souto@gmail.com>
2022-01-15 01:33:00 -0300

    Requisito 08: teste unitario 1 [resolvido]

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,13 @@
-const fetchProducts = async () => {
+const fetchProducts = async (product) => {
   // seu código aqui
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return (error);
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,13 @@
 require('../mocks/fetchSimulator');
+const { fetchItem } = require('../helpers/fetchItem');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    // referência colega @SrTonn e https://jestjs.io/pt-BR/docs/expect#tobeinstanceofclass
+    // testa se é objeto, função, array, string, etc;
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
 });
