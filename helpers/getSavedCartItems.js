5daff179aab45fbafd6766472143041d696751a0
Tabata <tabatac.souto@gmail.com>
2022-01-15 18:16:11 -0300

    Requisito 11: teste 01 [finalizado]

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,9 +1,4 @@
-const getSavedCartItems = () => {
-  // recuperada a section da ol;
-  const productCart = document.querySelector('.cart__items');
-  // acresecntado na section cart a ol salta em localStorage;
-  productCart.innerHTML = localStorage.getItem('cartItems');
-};
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
