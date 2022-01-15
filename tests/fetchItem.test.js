ed7108b846b9fc719ea9917d62b042301a35ea52
Tabata <tabatac.souto@gmail.com>
2022-01-15 12:33:11 -0300

    Requisito 09: teste 03 [resolvido]

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -12,8 +12,10 @@ describe('2 - Teste a função fecthItem', () => {
     expect(fetch).toHaveBeenCalled();
   });
 
-  it('', () => {
-    fail('Teste vazio');
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527".', async () => {
+    await fetchItem('MLB1615760527');
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 
   it('', () => {
