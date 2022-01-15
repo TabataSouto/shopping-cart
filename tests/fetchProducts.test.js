38a3eeb0a2acfae36c82a4bd4eea5077f6930799
Tabata <tabatac.souto@gmail.com>
2022-01-15 01:33:43 -0300

    Requisito 08: teste unitario 3 [resolvido]

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -16,4 +16,12 @@ describe('1 - Teste a função fecthProducts', () => {
     // toHaveBeenCalled garante que uma função de simulação (mock) foi chamada;
     expect(fetch).toHaveBeenCalled();
   });
+
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    await fetchProducts('computador');
+    // referência: https://jestjs.io/pt-BR/docs/expect#tohavebeencalledwitharg1-arg2-
+    // garante que uma função de simulação (mock) foi chamada com argumentos específicos;
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
